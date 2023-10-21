de5a993181a7763f63248cc909c444aeeb110b28
kodoklangit <kodoklangit.1@gmail.com>
2023-10-21 21:14:17 +0700

    delete mongodb on nodejs

--- backend-apps/nodejs-boilerplate/app.js
+++ backend-apps/nodejs-boilerplate/app.js
@@ -6,19 +6,6 @@ const bodyParser = require("body-parser");
 const restrictOrigin = require("./middlewares/restrictOrigin");
 
 const app = express();
-
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection Success!");
-  })
-  .catch((err) => {
-    console.error("Mongo Connection Error", err);
-  });
-
 const PORT = process.env.PORT || 5000;
 
 app.use(restrictOrigin);
